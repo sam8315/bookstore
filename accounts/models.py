b9d91fd3ed15ae13faa6c3adca2c8f640982fc4e
sam8315 <sam8315@gmail.com>
2023-12-20 14:46:47 +0330

    create CustomUser

---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
