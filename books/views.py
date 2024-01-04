bd5fe99d4992b382f9bac81681c1457b1528ca3c
sam8315 <sam.8315@gmail.com>
2024-01-04 17:00:53 +0330

    fasl 13

--- books/views.py
+++ books/views.py
@@ -5,6 +5,7 @@ from .models import Book
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 4
     template_name = 'books/book_list.html'
     context_object_name = 'books'
 
@@ -16,13 +17,13 @@ class BookDetailsView(generic.DetailView):
 
 class BookCreateView(generic.CreateView):
     model = Book
-    fields = ['title', 'author', 'content', 'price']
+    fields = ['title', 'author', 'content', 'price', 'cover']
     template_name = 'books/book_create.html'
 
 
 class BookUpdateView(generic.UpdateView):
     model = Book
-    fields = ['title', 'author', 'content', 'price']
+    fields = ['title', 'author', 'content', 'price', 'cover']
     template_name = 'books/book_update.html'
 
 
